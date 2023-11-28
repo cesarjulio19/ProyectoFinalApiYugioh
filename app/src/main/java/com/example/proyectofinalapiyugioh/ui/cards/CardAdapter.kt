93278ae1e87ca179b764ea64c9c26ddca6039a2c
cesarjulio19 <cesar.loky19@gmail.com>
2023-11-28 18:11:51 +0100

    commit

--- app/src/main/java/com/example/proyectofinalapiyugioh/ui/cards/CardAdapter.kt
+++ app/src/main/java/com/example/proyectofinalapiyugioh/ui/cards/CardAdapter.kt
@@ -1,4 +1,56 @@
 package com.example.proyectofinalapiyugioh.ui.cards
 
-class CardAdapter {
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import coil.imageLoader
+import coil.request.ImageRequest
+import com.example.proyectofinalapiyugioh.data.repository.Card
+import com.example.proyectofinalapiyugioh.databinding.CardItemBinding
+
+class CardAdapter(private val context: Context)
+    : RecyclerView.Adapter<CardAdapter.CardItemViewHolder>() {
+
+    private var cardList: List<Card> = emptyList()
+
+    inner class CardItemViewHolder(private val binding: CardItemBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(c: Card) {
+            binding.nameText.text = c.name
+
+            val imageRequest = ImageRequest.Builder(context)
+                .data(c.imageUrlSmall)
+                .crossfade(true)
+                .target(binding.image)
+                .build()
+
+            context.imageLoader.enqueue(imageRequest)
+
+        }
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CardItemViewHolder {
+        val binding = CardItemBinding.inflate(
+            LayoutInflater.from(parent.context),
+            parent,
+            false
+        )
+        return CardItemViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: CardItemViewHolder, position: Int) {
+        val card = cardList[position]
+        holder.bind(card)
+    }
+
+    override fun getItemCount(): Int {
+        return cardList.size
+    }
+
+    fun submitList(list: List<Card>) {
+        cardList = list
+        notifyDataSetChanged()
+    }
 }
--- app/src/main/java/com/example/proyectofinalapiyugioh/ui/cards/CardListFragment.kt
+++ app/src/main/java/com/example/proyectofinalapiyugioh/ui/cards/CardListFragment.kt
@@ -5,56 +5,43 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import com.example.proyectofinalapiyugioh.R
+import com.example.proyectofinalapiyugioh.databinding.FragmentCardListBinding
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [CardListFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
+@AndroidEntryPoint
 class CardListFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var binding: FragmentCardListBinding
+    private val viewModel:CardListViewModel by viewModels()
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_card_list, container, false)
+    ): View {
+        binding = FragmentCardListBinding.inflate(inflater,
+            container,
+            false)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment CardListFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            CardListFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val adapter = CardAdapter(requireContext())
+        val rv = binding.cardList
+        rv.adapter = adapter
+        viewLifecycleOwner.lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.uiState.collect {
+                    adapter.submitList(it.card)
                 }
             }
+        }
+
     }
 }
